9b768a8a9a556d2bd8bfc64b753d5263d06bfaa6
joonyeopyoon <yoon.jy@hanwha.com>
2023-10-19 16:46:30 +0900

    feat: admin

---
+++ src/admin/admin.module.ts
@@ -0,0 +1,48 @@
+import { Module } from '@nestjs/common';
+import AdminJS from 'adminjs';
+import * as AdminJSMongoose from '@adminjs/mongoose';
+import { AdminModule as AdminBroModule } from '@adminjs/nestjs';
+import { BoardModule } from 'src/board/board.module';
+import { getModelToken } from '@nestjs/mongoose';
+import { Board } from 'src/board/board.schema';
+import { Model } from 'mongoose';
+
+AdminJS.registerAdapter(AdminJSMongoose);
+
+@Module({
+  imports: [
+    AdminBroModule.createAdminAsync({
+      imports: [BoardModule],
+      inject: [getModelToken(Board.name)],
+      useFactory: (boardModel: Model<Board>) => ({
+        adminJsOptions: {
+          rootPath: '/admin',
+          resources: [
+            {
+              resource: boardModel,
+              options: {
+                properties: {
+                  contents: { type: 'richtext' },
+                },
+              },
+            },
+          ],
+          dashboard: {
+            component: AdminJS.bundle('./dashboard'),
+          },
+          branding: {
+            companyName: 'Amamov',
+            logo: false,
+          },
+        },
+        auth: {
+          authenticate: async (email, password) =>
+            Promise.resolve({ email: 'amamov@kakao.com' }),
+          cookieName: 'amamov',
+          cookiePassword: '1205',
+        },
+      }),
+    }),
+  ],
+})
+export class AdminModule {}
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,15 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BoardModule } from './Board/Board.module';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGO_URL),
+    BoardModule,
+    AdminModule,
   ],
   controllers: [AppController],
   providers: [AppService],
